b7d58e7d721713d469cb03ebd905e61b5b897cab
Simon <simon@lindenmann.net>
2021-07-16 11:25:11 +0200

    first commit

---
+++ src/main/java/com/example/commentarapp/CommentarAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.commentarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentarAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentarAppApplication.class, args);
+    }
+
+}
